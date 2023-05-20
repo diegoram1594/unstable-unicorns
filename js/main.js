f6eabddf17718709d122df6fd4f82012c9796f6d
Diego Ramirez <diegoramirez1594@gmail.com>
2023-05-19 19:38:59 -0500

    Category filter

--- js/main.js
+++ js/main.js
@@ -1,37 +1,41 @@
-var containerUnicorns = document.getElementById('container_unicorns');
+let unicornVisibility = true;
+let upgradeVisibility = true;
+let donwgradeVisibility = true;
+let containerUnicorns = document.getElementById('container_unicorns');
 
-var unicorns = JSON.parse(unicornData);
+let unicorns = JSON.parse(unicornData);
 unicorns.forEach(unicorn => {
     addCard(unicorn, containerUnicorns);
 });
 
-var containerUpgrades = document.getElementById('container_upgrades');
-var upgrades = JSON.parse(upgradeData);
+let containerUpgrades = document.getElementById('container_upgrades');
+let upgrades = JSON.parse(upgradeData);
 upgrades.forEach(upgrade => {
    addCard(upgrade, containerUpgrades);
 });
 
-var containerDowngrades = document.getElementById('container_downgrades');
-var downgrades = JSON.parse(downgradeData);
+let containerDowngrades = document.getElementById('container_downgrades');
+let downgrades = JSON.parse(downgradeData);
 downgrades.forEach(downgrade => {
    addCard(downgrade, containerDowngrades);
 });
 
 function addCard(item, container){
-    var imgCard = document.createElement("img");
+    let imgCard = document.createElement("img");
     imgCard.src = item.imgSrcSpanish;
     imgCard.className = "container_card";
     imgCard.id = item.id;
-    imgCard.onclick = function() { modalView(item.id) }
+    imgCard.onclick = function() { modalView(item.id) };
+    imgCard.loading = "lazy";
     container.appendChild(imgCard);
 }
 
-function searchContainer(container, list, separatorName){
+function searchContainer(container, list, separatorName, visibility){
     let text = document.getElementById("search").value.toLowerCase();
-    var separator = document.getElementById(separatorName);
-    var filterElements = list.filter(value => 
-        value.name.toLowerCase().includes(text) || 
-        value.nameSpanish.toLowerCase().includes(text));
+    let separator = document.getElementById(separatorName);
+    let filterElements = list.filter(value => 
+        (value.name.toLowerCase().includes(text) || 
+        value.nameSpanish.toLowerCase().includes(text)) &&  visibility);
     container.replaceChildren();
     filterElements.forEach(item => {
         addCard(item, container);
@@ -41,13 +45,35 @@ function searchContainer(container, list, separatorName){
     }
     else{
         separator.style.visibility = "visible";
-    }
-        
+    }   
 }
 
 function searchBar(){
-    searchContainer(containerUnicorns, unicorns, "separator_unicorn");
-    searchContainer(containerUpgrades, upgrades, "separator_upgrade");
-    searchContainer(containerDowngrades, downgrades, "separator_downgrade");
-    document
+    searchContainer(containerUnicorns, unicorns, "separator_unicorn", unicornVisibility);
+    searchContainer(containerUpgrades, upgrades, "separator_upgrade", upgradeVisibility);
+    searchContainer(containerDowngrades, downgrades, "separator_downgrade", donwgradeVisibility);
+}
+
+function categoryFilter(){
+    try{
+        let categoryUnicorn = document.getElementById('category_unicorn').checked;
+        let categoryUpgrade = document.getElementById('category_upgrade').checked;
+        let categoryInstant = document.getElementById('category_instant').checked;
+        let categoryDowngrade = document.getElementById('category_downgrade').checked;
+        let categoryMagic = document.getElementById('category_magic').checked;
+
+        if (!categoryUnicorn && !categoryUpgrade && !categoryInstant &&
+            !categoryDowngrade && !categoryMagic){
+            unicornVisibility = true;
+            upgradeVisibility = true;
+            donwgradeVisibility = true;
+            //TODO add all categories
+            return;
+        }
+        unicornVisibility = categoryUnicorn;
+        upgradeVisibility = categoryUpgrade;
+        donwgradeVisibility = categoryDowngrade;
+    } finally{
+        searchBar();
+    }
 }
